0732073b55167d5621f0dcee1e5e2b6b713f70f2
Sreelakshmi Anilkumar <sreelakshmita2015@gmail.com>
2021-06-22 18:46:03 +0530

    third commit

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,12 @@
-name = input("Enter your name : ")
-age = input("Enter your age : ")
-designation = input("Your current status : ")
-print("My name is", name)
+name = input("Enter your name :")
+age = input("Enter your age :")
+designation = input("Enter your designaton :")
+list = []
+for i in range(3):
+    skills = input("Enter your skills :")
+    list.append(skills)
+print("My name is",name)
 print("I'm",age,"years old")
 print("Currently I'm a",designation)
+print("I'm skilled in")
+print(*list, sep = ",")
